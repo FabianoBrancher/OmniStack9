a8b74aa82de19d6ddcf338610407e79e6c6959f8
Fabiano Brancher <fabiano.brancher@hotmail.com>
2019-10-06 16:12:49 -0300

    Added cors to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./ routes");
 
 const app = express();
@@ -17,7 +19,7 @@ mongoose.connect(
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
