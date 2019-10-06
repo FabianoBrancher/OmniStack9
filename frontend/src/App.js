2509eaa638433ded99f910aeaf6bafb96d19c162
Fabiano Brancher <fabiano.brancher@hotmail.com>
2019-10-06 17:57:44 -0300

    Frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import "./App.css";
 import React from "react";
 
+import logo from "./assets/logo.svg";
+
+import Routes from "./routes";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
+  );
 }
 
 export default App;
