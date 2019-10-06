19ba8a9c1d291df6070e2861f7aa58ebeb8bb3f5
Fabiano Brancher <fabiano.brancher@hotmail.com>
2019-10-06 16:40:35 -0300

    Added virtual url property to Spot model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const routes = require("./ routes");
 
@@ -21,6 +22,7 @@ mongoose.connect(
 // req.body = Acessar corpo da requisição (para criação, edição)
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
